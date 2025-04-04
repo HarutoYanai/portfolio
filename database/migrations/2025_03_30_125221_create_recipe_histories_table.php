c45d873913f1e550306b089c3fa54ceee3df390f
HarutoYanai <yanashimizu@outlook.jp>
2025-04-04 05:39:24 +0000

    for login

---
+++ database/migrations/2025_03_30_125221_create_recipe_histories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipe_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->bigInteger('rakuten_recipe_id');
+            $table->string('recipe_title');
+            $table->text('image_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipe_histories');
+    }
+};
